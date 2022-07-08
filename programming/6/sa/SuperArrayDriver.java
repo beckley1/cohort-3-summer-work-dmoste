b359623695593b9ca5d8baf35d64a8aee140d557
David Moste <60351405+dmoste@users.noreply.github.com>
2022-07-08 17:08:04 +0000

    small changes

--- programming/6/sa/SuperArrayDriver.java
+++ programming/6/sa/SuperArrayDriver.java
@@ -8,7 +8,6 @@ public class SuperArrayDriver
     System.out.println("Testing default constructor.");
     SuperArray sa = new SuperArray();
     System.out.println("Testing empty print:");
-    sa.debug();
     System.out.println(sa);
     System.out.println("Testing isEmpty (should be true)");
     System.out.println(sa.isEmpty());
