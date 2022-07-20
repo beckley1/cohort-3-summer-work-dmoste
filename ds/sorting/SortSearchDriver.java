bfe02f26da34baa0f080fe5aa57322d24003cfe4
David Moste <60351405+dmoste@users.noreply.github.com>
2022-07-20 13:30:14 +0000

    finished tests

--- ds/sorting/SortSearchDriver.java
+++ ds/sorting/SortSearchDriver.java
@@ -26,14 +26,17 @@ public class SortSearchDriver {
     	ss.sort();
     	System.out.println("Sorted: " + ss);
 
+      //Linear Search
       System.out.println("(Linear) Index of -2: " + ss.linearSearch(-2));
       System.out.println("(Linear) Index of 4: " + ss.linearSearch(4));
       System.out.println("(Linear) Index of 20: " + ss.linearSearch(20));
-      
+
+      //Binary Search
       System.out.println("Index of -2: " + ss.binarySearch(-2));
       System.out.println("Index of 4: " + ss.binarySearch(4));
       System.out.println("Index of 20: " + ss.binarySearch(20));
 
+      //Recursive Binary Search
       System.out.println("(Recursive) Index of -2: " + ss.binarySearchRecursive(-2, 0, 19));
       System.out.println("(Recursive) Index of 4: " + ss.binarySearchRecursive(4, 0, 19));
       System.out.println("(Recursive) Index of 20: " + ss.binarySearchRecursive(20, 0, 19));
