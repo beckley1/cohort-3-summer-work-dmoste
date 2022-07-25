2c9813c6f9cada8d9ac16aec369d8ea17aeaa059
David Moste <60351405+dmoste@users.noreply.github.com>
2022-07-25 14:38:28 +0000

    added merge

--- ds/sorting/SortSearchDriver.java
+++ ds/sorting/SortSearchDriver.java
@@ -29,18 +29,29 @@ public class SortSearchDriver {
     	System.out.println("Sorted: " + ss);
 
       //Linear Search
-      System.out.println("(Linear) Index of -2: " + ss.linearSearch(-2));
-      System.out.println("(Linear) Index of 11: " + ss.linearSearch(11));
-      System.out.println("(Linear) Index of 20: " + ss.linearSearch(20));
+      //System.out.println("(Linear) Index of -2: " + ss.linearSearch(-2));
+      //System.out.println("(Linear) Index of 11: " + ss.linearSearch(11));
+      //System.out.println("(Linear) Index of 20: " + ss.linearSearch(20));
 
       //Binary Search
-      System.out.println("Index of -2: " + ss.binarySearch(-2));
-      System.out.println("Index of 11: " + ss.binarySearch(11));
-      System.out.println("Index of 20: " + ss.binarySearch(20));
+      //System.out.println("Index of -2: " + ss.binarySearch(-2));
+      //System.out.println("Index of 11: " + ss.binarySearch(11));
+      //System.out.println("Index of 20: " + ss.binarySearch(20));
 
       //Recursive Binary Search
-      System.out.println("(Recursive) Index of -2: " + ss.binarySearchRecursive(-2, 0, size-1));
-      System.out.println("(Recursive) Index of 11: " + ss.binarySearchRecursive(11, 0, size-1));
-      System.out.println("(Recursive) Index of 20: " + ss.binarySearchRecursive(20, 0, size-1));
+      //System.out.println("(Recursive) Index of -2: " + ss.binarySearchRecursive(-2, 0, size-1));
+      //System.out.println("(Recursive) Index of 11: " + ss.binarySearchRecursive(11, 0, size-1));
+      //System.out.println("(Recursive) Index of 20: " + ss.binarySearchRecursive(20, 0, size-1));
+
+      System.out.println("---------------------------------------------");
+      ArrayList<Integer> a = ss.buildIncreasingList(20);
+	    System.out.println(a);
+
+	    // build a second Arraylist here
+      ArrayList<Integer> b = ss.buildIncreasingList(20);
+	    System.out.println(b);
+
+	    // test your merge routine here
+      System.out.println(ss.merge(a,b));
     }
 }
