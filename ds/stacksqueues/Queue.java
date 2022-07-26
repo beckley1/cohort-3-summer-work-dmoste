4d57a0b1d21faa4649d50b694e7a25a48dfcf80f
David Moste <60351405+dmoste@users.noreply.github.com>
2022-07-26 16:30:32 +0000

    stack + queue outlines

--- ds/stacksqueues/Queue.java
+++ ds/stacksqueues/Queue.java
@@ -1,10 +1,41 @@
+
 public class Queue{
 
-    // Place private instance variables here
+  // Place private instance variables here
 
-    // Place constructors here
+  // Place constructors here - A constructor with no parameters
 
-    // Place methods here
+  // Place methods here
+
+  // void enqueue(int value) - add value to the end of the queue
+  public void enqueue(int value){
+    
+  }
+
+  // int dequeue() - remove and return the top value from front of the queue
+  public int dequeue(){
+    
+  }
+
+  // int front() - return but do not remove the top value from the front of the queue
+  public int front(){
+    
+  }
+
+  // boolean isEmpty() - return true of the queue is empty, false otherwise
+  public boolean isEmpty(){
+    
+  }
+
+  // int size() - return the number of elements currently in the queue
+  public int size(){
+    
+  }
+
+  // boolean isFull() - returns true if the queue is full, false otherwise - only implement this if you use an array for implementation
+  public boolean isFull(){
+    
+  }
     
 
 }
--- ds/stacksqueues/Stack.java
+++ ds/stacksqueues/Stack.java
@@ -1,10 +1,41 @@
+
+
 public class Stack{
 
-    // Place private instance variables here
+  // Place private instance variables here
 
-    // Place constructors here
+  // Place constructors here - A constructor with no parameters
 
-    // Place methods here
+  // Place methods here
+
+  // void push(int value) - add value to the top of the stack
+  public void push(int value){
     
+  }
+
+  // int pop() - remove and return the top value from the stack
+  public int pop(){
+    
+  }
+
+  // int top() - return but do not remove the top value from the stack
+  public int top(){
+    
+  }
+
+  // boolean isEmpty() - return true of the stack is empty, false otherwise
+  public boolean isEmpty(){
+    
+  }
+
+  // int size() - return the number of elements currently in the stack
+  public int size(){
+    
+  }
+
+  // boolean isFull() - returns true if the stack is full, false otherwise - only implement this if you use an array for implementation
+  public boolean isFull(){
+    
+  }
 
 }
