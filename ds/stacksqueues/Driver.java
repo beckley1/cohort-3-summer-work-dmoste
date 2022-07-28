7534fca88f30849444131c237db4b93c2769d48c
David Moste <60351405+dmoste@users.noreply.github.com>
2022-07-28 17:29:06 +0000

    changed driver print

--- ds/stacksqueues/Driver.java
+++ ds/stacksqueues/Driver.java
@@ -9,11 +9,11 @@ public class Driver{
     
     System.out.println("Stack Empty: " + s.isEmpty());
     System.out.println(s);
-    System.out.println("Adding 5 to Stack");
+    System.out.println("Pushing 5 to Stack");
     s.push(5);
-    System.out.println("Adding 10 to Stack");
+    System.out.println("Pushing 10 to Stack");
     s.push(10);
-    System.out.println("Adding 12 to Stack");
+    System.out.println("Pushing 12 to Stack");
     s.push(12);
     System.out.println("Stack Empty: " + s.isEmpty());
     System.out.println(s);
@@ -22,9 +22,9 @@ public class Driver{
     System.out.println(s);
     System.out.println("Current Stack Size: " + s.size());
     System.out.println("Stack Full? " + s.isFull());
-    System.out.println("Adding 2 to Stack");
+    System.out.println("Pushing 2 to Stack");
     s.push(2);
-    System.out.println("Adding 124 to Stack");
+    System.out.println("Pushing 124 to Stack");
     s.push(124);
     System.out.println(s);
 
