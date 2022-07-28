b734c7323b92267950173beaffe7b9cdd28b7aa3
David Moste <60351405+dmoste@users.noreply.github.com>
2022-07-28 17:16:36 +0000

    finalized stack

--- ds/stacksqueues/Driver.java
+++ ds/stacksqueues/Driver.java
@@ -6,22 +6,24 @@ public class Driver{
   public static void main(String[] args) {
 
 	  Stack s = new Stack();
+    
+    System.out.println("Stack Empty: " + s.isEmpty());
     s.push(5);
     s.push(10);
     s.push(12);
     System.out.println("Stack Empty: " + s.isEmpty());
-    System.out.println("Current state of stack:");
-    System.out.println(s);
-    System.out.println("Popped stack: " + s.pop());
-    System.out.println("Top of stack: " + s.top());
-    System.out.println("Current state of stack:");
-    System.out.println(s);
+    System.out.println("\n---Current State of Stack---");
+    System.out.println(s + "\n");
+    System.out.println("Popped Stack: " + s.pop());
+    System.out.println("Top of Stack: " + s.top());
+    System.out.println("\n---Current State of Stack---");
+    System.out.println(s + "\n");
     System.out.println("Current Stack Size: " + s.size());
     System.out.println("Stack Full? " + s.isFull());
     s.push(2);
     s.push(124);
-    System.out.println("Current state of stack:");
-    System.out.println(s);
+    System.out.println("\n---Current State of Stack---");
+    System.out.println(s + "\n");
 
 	  //Queue q = new Queue();
 	
