ecc5450abf58f2a9048924e37860c4c77352f087
David Moste <60351405+dmoste@users.noreply.github.com>
2022-08-01 17:36:10 +0000

    added left.py

---
+++ methods/11_lessons/lesson_00/left.py
@@ -0,0 +1,11 @@
+def left(currentState):
+  if currentState == "↓":
+    print("←")
+  elif currentState == "←":
+    print("↑")
+  elif currentState == "↑":
+    print("→")
+  elif currentState == "→":
+    print("↓")
+
+left()
